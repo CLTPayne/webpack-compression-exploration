f0433789bc1dd12e9a386a9f011edf4443bd3a78
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-09-29 14:11:17 +0100

    Bash script to determin Content-Type property
    * Works for html, css and js

--- scripts/uploadToS3.sh
+++ scripts/uploadToS3.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 #
-# Upload all webpack emitted build assets to Amazon S3 buckets
-#
+# Upload all webpack emitted build assets to Amazon S3 buckets with correct 
+# Content-Type key value pair for processing in the browswer post decompression
 
 # Set error handling
 set -eu -o pipefail
+
+# Calculate original Content-Type
+get_content_type() {
+    file=$1
+    file_extension=${file##*.}
+    echo "Current extension of $file => .$file_extension"
+    declare extension
+    if [[ "$file_extension" == "br" || "$file_extension" == "gz" ]]
+    then 
+        original_filename=${file%.*}
+        extension=${original_filename##*.}
+        echo -e "\x1b[36mOriginal extension of $file => .$extension\x1b[0m"
+    elif [[ ! "$file_extension" == "br" || !"$file_extension" == "gz" ]]
+    then 
+        extension=$file_extension
+        echo -e "\x1b[36mOriginal extension of $file => .$extension\x1b[0m"
+    fi
+    # TODO: Default Content-Type in S3 - should this be applied?
+    # or does that over ride the correct value for source maps etc
+    declare content_type
+    if [[ "$extension" == "html" ]]; then content_type="text/html; charset=utf-8"; fi
+    if [[ "$extension" == "css" ]]; then content_type="text/css; charset=utf-8"; fi
+    if [[ "$extension" == "js" ]]; then content_type="application/javascript"; fi
+    echo -e "\033[31mContent-Type of $file => "$content_type"\x1b[36m"
+}
 
 # Set build directory
 DIRECTORY="./dist"
@@ -14,7 +39,15 @@ if [ -d "$DIRECTORY" ]
 then 
     echo "Built files found in ./dist folder"
     cd "$DIRECTORY"
-    ls
+    files=(*)
+    echo "${#files[@]} files in the $DIRECTORY to be processed" # will echo number of files in array
+    delim=", "
+    echo -e "Files to be processed are: \033[31m${files[@]}\x1b[36m" # will list all files of the array
+    for file in "${files[@]}"; do  
+        get_content_type "$file"
+        # TODO: Upload to S3 storage bucket with AWS CLI
+        # e.g. node_modules/.bin/aws s3 $file --content-type 
+    done 
 elif [ ! -d "$DIRECTORY" ]
 then
     echo "$DIRECTORY folder not found"
